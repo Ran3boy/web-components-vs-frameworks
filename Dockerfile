f840aea6be951538785d4bfaa3f1b16e9636dc7a
Ran3boy <ivanov11022012@gmail.com>
2026-04-26 21:06:05 +0300

    Fix: исправлена структура папок и настройка Vite для корректного отображения

--- Dockerfile
+++ Dockerfile
@@ -11,6 +11,8 @@ RUN npm run build
 
 # Этап 2: Раздача статики через Nginx
 FROM nginx:alpine
+
 COPY --from=build /app/dist /usr/share/nginx/html
+
 EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/web-components-vs-frameworks/',
+  base: './',
   plugins: [react()],
 });
