3527e4cb3958623b3edea266ab1b0bee087e6c99
Ran3boy <ivanov11022012@gmail.com>
2026-04-26 22:38:44 +0300

    Fix-1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: '/web-components-vs-frameworks/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/web-components-vs-frameworks/' : '/',
   plugins: [react()],
-});
+}));
